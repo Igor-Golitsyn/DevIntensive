83b2df825d2a19d0380828470368c3b08e7081d1
Igor Golitsyn <polus79@mail.ru>
2016-07-05 10:31:35 +0500

    hometask_4 completed доработан контроль валидности данных

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -151,6 +151,30 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 }
             }
         });
+        mUserMail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!checkEmail()) showToast(getString(R.string.wrong_email));
+            }
+        });
+        mUserPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!checkPhoneNumber())showToast(getString(R.string.wrong_phone_number));
+            }
+        });
 
         mUserInfoViews = new ArrayList<>();
         mUserInfoViews.add(mUserPhone);
